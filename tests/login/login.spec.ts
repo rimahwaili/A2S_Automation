48f1df015c1ae72187e0e1c8561e20257a460011
rimahwaili <rimah.waili@consulting-for.accor.com>
2025-11-24 22:09:20 +0100

    add contact archive tests

--- tests/login/login.spec.ts
+++ tests/login/login.spec.ts
@@ -4,15 +4,17 @@ import { credentials } from '../../utils/env';
 import { loginSelectors } from '../../selectors/login.selectors';
 import { translations, getLang } from '../../utils/translations';
 import { allure } from 'allure-playwright';
+import { ForgotPasswordPage } from '../../pages/ForgotPasswordPage';
 
 
 const lang = getLang(); // détecte depuis process.env.LANG
 const t = translations[lang];
 
+let forgotPassword: ForgotPasswordPage;
 
 test.describe('Login Tests', () => {
   test('A2SQA2-2522 | @P0 @Login Login with valid credentials', async ({ page }) => {
-
+  forgotPassword = new ForgotPasswordPage(page);
     allure.label('feature', 'Login');
     allure.epic('Login');
     allure.story('Login with valid credentials');
@@ -23,7 +25,11 @@ test.describe('Login Tests', () => {
     const t = translations[loginPage.lang];
 
     await loginPage.login(credentials.username, credentials.password);
+    const currentUrl = page.url();
 
+    if (currentUrl.includes('users/cgu_required')) {
+    await forgotPassword.fillNewPassword('Tuesday@260994!!', 'Tuesday@260994!!');
+  }
     const toast = page.locator(loginSelectors.successToast);
     await expect(toast).toBeVisible();
      await expect(toast.locator(loginSelectors.toastHeader)).toContainText(/Succès|Success/i);
