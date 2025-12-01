56c15da3b471d7c43e1624072080751f72acd613
rimahwaili <rimahwaili@users.noreply.github.com>
2025-12-01 19:40:15 +0100

    🗂️ Update Allure report index (cron) 9f793ff2e42b436a8b95325c1962c3a0bf5e8272

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 3 : 0,
+  retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : 1,
   reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }],["line"], ["allure-playwright"]],
    use: {
